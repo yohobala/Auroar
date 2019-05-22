dd007e9cf62e91ccdc24485f72866836fe00ea6c
黄灏 <762100246@qq.com>
2019-05-22 21:46:05 +0800

    更新

--- app/src/main/java/hh/auroar/MainActivity.java
+++ app/src/main/java/hh/auroar/MainActivity.java
@@ -3,11 +3,17 @@ package hh.auroar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.CoordType;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BaiduMapOptions;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.*;
+import com.baidu.mapapi.map.MyLocationData;
 
 public class MainActivity extends AppCompatActivity {
     private MapView mMapView = null;
@@ -21,8 +27,51 @@ public class MainActivity extends AppCompatActivity {
         //获取地图
         mMapView = new  MapView(this, options);
         mMapView =(MapView) findViewById(R.id.bmapView);
-        //开启定位
     }
+    private void initLocationOption(){
+        //定位服务的客户端。宿主程序在客户端声明此类，并调用，目前只支持在主线程中启动
+        LocationClient locationClient = new LocationClient(getApplicationContext());
+        //声明LocationClient类实例并配置定位参数
+        LocationClientOption locationOption = new LocationClientOption();
+          //可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        locationOption.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+          //可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
+        locationOption.setCoorType("gcj02");
+          //可选，默认0，即仅定位一次，设置发起连续定位请求的间隔需要大于等于1000ms才是有效的
+        locationOption.setScanSpan(1000);
+          //可选，设置是否需要地址信息，默认不需要
+        locationOption.setIsNeedAddress(true);
+          //可选，设置是否需要地址描述
+        locationOption.setIsNeedLocationDescribe(true);
+          //可选，设置是否需要设备方向结果
+        locationOption.setNeedDeviceDirect(false);
+          //可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
+        locationOption.setLocationNotify(true);
+          //可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+        locationOption.setIgnoreKillProcess(true);
+          //可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        locationOption.setIsNeedLocationDescribe(true);
+          //可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        locationOption.setIsNeedLocationPoiList(true);
+          //可选，默认false，设置是否收集CRASH信息，默认收集
+        locationOption.SetIgnoreCacheException(false);
+          //可选，默认false，设置是否开启Gps定位
+        locationOption.setOpenGps(true);
+         //可选，默认false，设置定位时是否需要海拔信息，默认不需要，除基础定位版本都可用
+        locationOption.setIsNeedAltitude(false);
+        //注册监听函数
+        MyLocationListener myLocationListener = new MyLocationListener();
+        locationClient.registerLocationListener(myLocationListener);
+        //设置打开自动回调位置模式，该开关打开后，期间只要定位SDK检测到位置变化就会主动回调给开发者，该模式下开发者无需再关心定位间隔是多少，定位SDK本身发现位置变化就会及时回调给开发者
+        locationOption.setOpenAutoNotifyMode();
+        //设置打开自动回调位置模式，该开关打开后，期间只要定位SDK检测到位置变化就会主动回调给开发者
+        locationOption.setOpenAutoNotifyMode(3000,1, LocationClientOption.LOC_SENSITIVITY_HIGHT);
+//需将配置好的LocationClientOption对象，通过setLocOption方法传递给LocationClient对象使用
+        locationClient.setLocOption(locationOption);
+//开始定位
+        locationClient.start();
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -40,6 +89,8 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
         mMapView.onDestroy();
+        mMapView = null;
     }
 }
+
 
---
+++ app/src/main/java/hh/auroar/MyLocationListener.java
@@ -0,0 +1,24 @@
+package hh.auroar;
+
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+
+public class MyLocationListener extends BDAbstractLocationListener {
+    @Override
+    public void onReceiveLocation(BDLocation location){
+        //此处的BDLocation为定位结果信息类，通过它的各种get方法可获取定位相关的全部结果
+        //以下只列举部分获取经纬度相关（常用）的结果信息
+        //更多结果信息获取说明，请参照类参考中BDLocation类中的说明
+
+        //获取纬度信息
+        double latitude = location.getLatitude();
+        //获取经度信息
+        double longitude = location.getLongitude();
+        //获取定位精度，默认值为0.0f
+        float radius = location.getRadius();
+        //获取经纬度坐标类型，以LocationClientOption中设置过的坐标类型为准
+        String coorType = location.getCoorType();
+        //获取定位类型、定位错误返回码，具体信息可参照类参考中BDLocation类中的说明
+        int errorCode = location.getLocType();
+    }
+}
