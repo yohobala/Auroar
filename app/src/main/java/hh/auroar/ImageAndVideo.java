dd3b4e9b336a3f89a9fdf20870823b2b4434fa44
黄灏 <762100246@qq.com>
2019-05-28 18:13:01 +0800

    更新

--- app/src/main/java/hh/auroar/ImageAndVideo.java
+++ app/src/main/java/hh/auroar/ImageAndVideo.java
@@ -1,40 +1,133 @@
 package hh.auroar;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ImageSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.io.File;
+
 public class ImageAndVideo extends AppCompatActivity {
-    public ImageButton edit;
-    public TextView Text;
-    public LinearLayout linearLayout;
-    public LinearLayout linearLayout2;
-    public RecyclerView RecyclerView;
+    public EditText editText;
+    public Button test;
+    public Button button;
+    public ImageView imageView;
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE2 = 7;
+    private static final int REQUEST_CODE_PICK_IMAGE = 3;
+    private File output;
+    private Uri imageUri;
+    private static final int CROP_PHOTO = 2;
+    private String str=null;
+    private SpannableString ss;
+    public String path;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.input);
-        linearLayout=findViewById(R.id.linearLayout);
-        edit=findViewById(R.id.imageButton);
-        Text=findViewById(R.id.textView);
-        Edit();
+        setContentView(R.layout.edit);
+        editText = findViewById(R.id.edit);
+        button = findViewById(R.id.button);
+        test = findViewById(R.id.button2);
+        imageView=findViewById(R.id.imageView);
+        test();
 
-        Intent intent =getIntent();
-        Text.setText(intent.getStringExtra("回传"));
     }
-    public void Edit(){
-     edit.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View v) {
-             Intent i=new Intent(ImageAndVideo.this, Edit.class);
-             startActivity(i);
-         }
-     });
+    public void test() {
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(Edit.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(Edit.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            MY_PERMISSIONS_REQUEST_CALL_PHONE2);
+
+                    //权限还没有授予，需要在这里写申请权限的代码
+                } else {
+                    //权限已经被授予，在这里直接写要执行的相应方法即可
+                    choosePhoto();
+
+                }
+            }
+        });
     }
+
+    void choosePhoto() {
+        /**
+         * 打开选择图片的界面
+         */
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("image/*");//相片类型
+        startActivityForResult(intent, REQUEST_CODE_PICK_IMAGE);
+
+    }
+
+    public void onActivityResult(int req, int res, Intent data) {
+
+        switch (req) {
+            /**
+             * 从相册中选取图片的请求标志
+             */
+
+            case REQUEST_CODE_PICK_IMAGE:
+                //if (res == RESULT_OK) {
+                try {
+                    /**
+                     * 该uri是上一个Activity返回的
+                     */
+                    Uri uri = data.getData();
+                    Bitmap bit = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
+
+                    Editable edit_text = getEditableText();
+                    int index = getSelectionStart(); // 获取光标所在位置
+                    //插入换行符，使图片单独占一行
+                    SpannableString newLine = new SpannableString("\n");
+                    edit_text.insert(index, newLine);
+                    // 创建一个SpannableString对象，以便插入用ImageSpan对象封装的图像
+                    path = mBitmapTag + path + mBitmapTag;
+                    SpannableString spannableString = new SpannableString(path);
+                    // 根据Bitmap对象创建ImageSpan对象
+                    ImageSpan imageSpan = new ImageSpan(mContext, bitmap);
+                    // 用ImageSpan对象替换你指定的字符串
+                    spannableString.setSpan(imageSpan, 0, path.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    // 将选择的图片追加到EditText中光标所在位置
+                    if (index < 0 || index >= edit_text.length()) {
+                        edit_text.append(spannableString);
+                    } else {
+                        edit_text.insert(index, spannableString);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    // Log.d("tag", e.getMessage());
+                    //Toast.makeText(this, "程序崩溃", Toast.LENGTH_SHORT).show();
+                }
+                //} else {
+                //Log.i("liang", "失败");
+                //}
+
+                break;
+
+            default:
+                break;
+        }
+    }
+
 }
